f204243bf39b3dacd93e1b21852ca5e9c8f1a2b4
eurekasolution <nadopro@gmail.com>
2022-04-25 10:50:23 +0900

    2022-04-25 10:50

---
+++ src/main/java/com/kbstar/springboot/study/web/IndexController.java
@@ -0,0 +1,23 @@
+package com.kbstar.springboot.study.web;
+/*
+34 IndexController.java
+    http://kbstar.com/
+ */
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class IndexController {
+    @GetMapping("/")
+    public String index()
+    {
+        return "index";
+    }
+}
+
+/*
+    http://kbstar.com/
+    query가 없으면 index를 리턴한다.
+        index.mustache 파일이 있으면 이 파일이 index파일을 대체한다.
+ */
