51340a00a82790b2bab7422e368d3fb9c95d6f9c
eurekasolution <nadopro@gmail.com>
2022-05-02 10:32:54 +0900

    2022-05-02 10:32

--- src/main/java/com/kbstar/springboot/study/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/kbstar/springboot/study/config/auth/dto/OAuthAttributes.java
@@ -26,12 +26,14 @@ public class OAuthAttributes {
         this.picture = picture;
     }
 
+    // Google
     public static OAuthAttributes of(String registrationId,
                                      String userNameAttributeName,
                                      Map<String, Object> attributes) {
         return ofGoogle(userNameAttributeName, attributes);
     }
 
+    // Google
     private static OAuthAttributes ofGoogle(String userNameAttributeName,
                                             Map<String, Object> attributes) {
         return OAuthAttributes.builder()
